209088f157d000fe3ae84650ed6d2b0c4e941dc1
suhaasbadada <suhaasbadada@gmail.com>
2022-09-29 21:31:47 +0530

    section3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,39 +5,9 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent{
   title='any value';
   display = true;
-  moviesInTheatres:any;
-  moviesFutureReleases:any;
-  
-  ngOnInit(): void{
-    setTimeout(()=>{
-    this.moviesInTheatres=[{
-      title:'Spider-Man',
-      releaseDate: new Date(),
-      lead: 'Toby Maguire, James Franco, Kirsten Dunst, Willem Dafoe',
-      director: 'sam raimi',
-      tprice: 300.51
-    }];
-    
-
-    this.moviesFutureReleases=[{
-      title:'Spider-Man 2 ',
-      releaseDate: new Date(),
-      lead: 'Toby Maguire, James Franco, Kirsten Dunst, Willem Dafoe',
-      director: 'sam raimi',
-      tprice: 300.51
-    },
-    {
-      title:'Spider-Man 3',
-      releaseDate: new Date(),
-      lead: 'Toby Maguire, James Franco, Kirsten Dunst, Willem Dafoe',
-      director: 'sam raimi',
-      tprice: 300.51
-    }];
-  },500)
-  }
 
   doubleNumber(n: number){
     return n*2 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { MenuComponent } from './menu/menu.component';
 import { RatingComponent } from './utilies/rating/rating.component';
-import { LifecycletestComponent } from './lifecycletest/lifecycletest.component';
+import { HomeComponent } from './home/home.component';
+import { IndexGenresComponent } from './genres/index-genres/index-genres.component';
+import { CreateGenreComponent } from './genres/create-genre/create-genre.component';
+import { IndexActorsComponent } from './actors/index-actors/index-actors.component';
+import { CreateActorComponent } from './actors/create-actor/create-actor.component';
+import { IndexMovieTheaterComponent } from './movie-theaters/index-movie-theater/index-movie-theater.component';
+import { CreateMovieTheaterComponent } from './movie-theaters/create-movie-theater/create-movie-theater.component';
+import { CreateMovieComponent } from './movies/create-movie/create-movie.component';
+import { EditActorComponent } from './actors/edit-actor/edit-actor.component';
+import { EditGenreComponent } from './genres/edit-genre/edit-genre.component';
+import { EditMovieTheaterComponent } from './movie-theaters/edit-movie-theater/edit-movie-theater.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +28,17 @@ import { LifecycletestComponent } from './lifecycletest/lifecycletest.component'
     GenericListComponent,
     MenuComponent,
     RatingComponent,
-    LifecycletestComponent
+    HomeComponent,
+    IndexGenresComponent,
+    CreateGenreComponent,
+    IndexActorsComponent,
+    CreateActorComponent,
+    IndexMovieTheaterComponent,
+    CreateMovieTheaterComponent,
+    CreateMovieComponent,
+    EditActorComponent,
+    EditGenreComponent,
+    EditMovieTheaterComponent
   ],
   imports: [
     BrowserModule,
